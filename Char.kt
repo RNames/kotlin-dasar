4786201dee3db337ba5a9a9101851b19d005bc36
RNames <124018774+RNames@users.noreply.github.com>
2023-09-07 22:48:33 +0700

    Add files via upload

---
+++ Char.kt
@@ -0,0 +1,34 @@
+fun main() {
+//      Characters are represented by the type Char. Character literals go in single quotes: '1'.
+//      Special characters start from an escaping backslash "\"
+
+//      SPECIAL CHARACTER :
+//      \t – tab
+
+//      \b – backspace
+
+//      \n – new line (LF)
+
+//      \r – carriage return (CR)
+
+//      \' – single quotation mark
+
+//      \" – double quotation mark
+
+//      \\ – backslash
+
+//      \$ – dollar sign
+
+    val dChar: Char = 'D' // Need a single quotation mark instead of double
+    val aChar: Char = 'A'
+    val fChar: Char = 'F'
+
+
+    print(dChar);print('\n')
+    print(aChar);print('\n')
+    print(fChar);print('\n')
+    print(fChar);print('\n')
+    print(aChar)
+
+
+}
